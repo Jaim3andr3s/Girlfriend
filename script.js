64ca5dadf272d9ff9d508a6e8fed576069a5f582
Jaime <jgilamaris49@gmail.com>
2024-05-26 13:00:29 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const yesBtn = document.querySelector('.yes-btn');
+    const noBtn = document.querySelector('.no-btn');
+
+    // Asignar función de clic al botón "Sí"
+    yesBtn.addEventListener('click', function() {
+        alert("Chapeeeee.");
+    });
+
+    // Asignar evento de movimiento al botón "No"
+    noBtn.addEventListener('mouseover', function() {
+        moveButton(noBtn);
+    });
+
+    function moveButton(button) {
+        const rect = button.getBoundingClientRect(); // Obtener el rectángulo del botón
+        const maxX = window.innerWidth - rect.width; // Calcular el máximo de X
+        const maxY = window.innerHeight - rect.height; // Calcular el máximo de Y
+
+        const randomX = Math.floor(Math.random() * maxX);
+        const randomY = Math.floor(Math.random() * maxY);
+
+        button.style.left = randomX + 'px';
+        button.style.top = randomY + 'px';
+    }
+});
